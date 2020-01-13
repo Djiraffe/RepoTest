8c12dc02bfb0547a30de966c5af26dccf80cf838
hassan24 <hassan24@98E74366A611.shernet.sheridancollege.ca>
2020-01-13 11:04:54 -0500

    My First commit

---
+++ src/ca/sheridancollege/week2/cardexample/card.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week2.cardexample;
+
+/**
+ * A simple java card class.
+ * @author Ali Hassan
+ */
+public class card {
+    private String suit;
+    private int value;
+
+    /**
+     * A constructor to create card class.
+     * @param suit
+     * @param value 
+     */
+    public card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        
+        if(suit.equals("Spades")||suit.equals("Diamond")||
+                suit.equals("Clubs")||suit.equals("Hearts"))
+            this.suit = suit;
+        else{
+            System.out.println("Incorrect suit. Please enter a valid suit");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        if(value>0 && value<14)
+            this.value = value;
+        else
+            System.out.println("Incorrect value. Please enter a value card value.");
+    }
+    
+    
+}
---
+++ src/ca/sheridancollege/week2/cardexample/deckOfCards.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week2.cardexample;
+
+/**
+ * A simple class to create deck of cards.
+ *
+ * @author Ali Hassan
+ */
+public class deckOfCards {
+
+    public static void main(String[] args) {
+        card tenOfHearts = new card("Hearts", 150);
+        tenOfHearts.setValue(200);   
+    }
+
+}
