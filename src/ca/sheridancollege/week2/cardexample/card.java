6aadd0b1417048a2ec9707f305848ded9e506b95
hassan24 <hassan24@98E74366A611.shernet.sheridancollege.ca>
2020-01-13 11:05:20 -0500

    My second commit

--- src/ca/sheridancollege/week2/cardexample/card.java
+++ src/ca/sheridancollege/week2/cardexample/card.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Yet another line added.
 package ca.sheridancollege.week2.cardexample;
 
 /**
